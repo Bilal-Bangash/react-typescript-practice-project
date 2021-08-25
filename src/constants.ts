e2c53d29bf1a1ffb518f18e0b5e7e883bcb31600
Bilal-Bangash <muhammadbilalbangash@hotmail.com>
2021-08-25 16:18:24 +0500

    fetch random paragraph from quotable.io

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,5 @@ export const SIGN_IN_ROUTE = '/sign-in'
 export const TYPING_TEST_ROUTE = '/typing-test'
 export const HOME_ROUTE = '/home'
 export const SCORECARD_ROUTE = '/scorecard'
+export const RANDOM_PARAGRAPH_URL =
+  'https://api.quotable.io/random?minLength=300'
